48fd2563ad21bf88df90b44c5cc682da8cc2461c
Lau <lau.mc84@gmail.com>
2018-10-07 20:09:21 +0200

    componente pokedex -not work

--- src/App.js
+++ src/App.js
@@ -1,47 +1,41 @@
 import React, { Component } from 'react';
 import './App.css';
-// import Pokemon from "./components/Pokemon";
+import Pokedex from './components/Pokedex';
 
 const apiPoke = 'https://pokeapi.co/api/v2/pokemon/';
 const countPokemons = 5;
 
 class App extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
       pokemonList: [],
-      pokemon: null
+      pokemonSearch: ""
     }
   }
-  componentDidMount() {
-    this.fetching()
+
+  componentDidMount(){
+    this.fetching();
   }
-  
-  fetching(){
+
+  fetching() {
     fetch(apiPoke)
-    .then(response => response.json())
+      .then(response => response.json())
 
-    .then(data => {
-      let list = data.results;
-      this.saveList(list);
-    })
+      .then(data => {
+        let list = data.results;
+        for (let i = 0; i < countPokemons; i++) {
+          fetch(list[i].url)
+            .then(response => {
+              return response.json();
+            })
+            .then(data => {
+              this.setState({pokemonList: [...this.state.pokemonList, data]})
+            })
+        }
+      })
   }
 
-  saveList(pokemons){
-    let pokeList = [];
-    
-    for(let i=0; i<countPokemons; i++){
-      fetch(pokemons[i].url)
-      .then (response => {
-        return response.json();
-      })
-      .then (data => {
-        pokeList.push(data);
-      })
-    }
-    this.setState({pokemonList: pokeList})
-  }
-      
 
   render() {
     return (
@@ -50,28 +44,8 @@ class App extends Component {
           <h1 className="appName">Tu Pokedex</h1>
         </header>
         <main>
-        <input type="text"/>
-        <ul className="pokeList">
-          {
-            this.state.pokemonList.map(pokemon => {
-              return (
-                <li className="card">
-                  <div className="cardContainer">
-                    <div className="cardContailer__ImageContainer">
-                      <img src={pokemon.sprites.front_default} alt={pokemon.name}/>
-                      <span className="pokemonOrder">{pokemon.order}/{countPokemons}</span>
-                    </div>
-                    <div className="cardContailer__dataContainer">
-                      <div className="cardContailer__dataContainer--type">
-
-                      </div>
-                    </div>
-                  </div>
-                </li>
-              )
-            })
-          }
-        </ul>
+          <input type="text" />
+          <Pokedex pokemonList={this.state.pokemonList}/>
         </main>
       </div>
     );
