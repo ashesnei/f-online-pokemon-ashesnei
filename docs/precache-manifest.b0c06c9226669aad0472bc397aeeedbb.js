9ebc99f8412ed45a11ffc81df4ef3d942e4e20dc
Lau <lau.mc84@gmail.com>
2018-10-09 00:12:40 +0200

    build

---
+++ docs/precache-manifest.b0c06c9226669aad0472bc397aeeedbb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "5b8a17c4a72ec7e3bafd",
+    "url": "./static/js/main.5b8a17c4.chunk.js"
+  },
+  {
+    "revision": "e9feb2fbf639035fda66",
+    "url": "./static/js/1.e9feb2fb.chunk.js"
+  },
+  {
+    "revision": "5b8a17c4a72ec7e3bafd",
+    "url": "./static/css/main.f402eb14.chunk.css"
+  },
+  {
+    "revision": "fb111e312fb4f729f7535619cf8bccc8",
+    "url": "./index.html"
+  }
+];
